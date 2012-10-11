90e3dc23c6008ccb0295d6ad2f4e9069c7aaab32
Don Brown <mrdon@twdata.org>
2012-10-11 12:52:37 -0600

    Workaround for REST version that broke API

--- plugin/src/main/java/com/atlassian/labs/speakeasy/util/JsonObjectMapper.java
+++ plugin/src/main/java/com/atlassian/labs/speakeasy/util/JsonObjectMapper.java
@@ -4,11 +4,15 @@ import com.atlassian.labs.speakeasy.model.JsonManifest;
 import com.atlassian.plugins.rest.common.json.JacksonJsonProviderFactory;
 import org.apache.commons.io.IOUtils;
 import org.apache.poi.hssf.record.formula.functions.T;
+import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import javax.ws.rs.core.MediaType;
 import java.io.*;
+import java.lang.reflect.InvocationTargetException;
+
+import static java.util.Collections.emptyList;
 
 /**
  *
@@ -30,7 +34,8 @@ public class JsonObjectMapper
         {
             // this is lame but needed to get it to compile
             Class<Object> foo = (Class<Object>) JsonObjectMapper.class.getClassLoader().loadClass(objectType.getName());
-            return (T) new JacksonJsonProviderFactory().create().readFrom(foo, objectType, null, MediaType.APPLICATION_JSON_TYPE, null, in);
+
+            return (T) createProvider().readFrom(foo, objectType, null, MediaType.APPLICATION_JSON_TYPE, null, in);
         }
         catch (ClassNotFoundException e)
         {
@@ -40,6 +45,40 @@ public class JsonObjectMapper
         {
             IOUtils.closeQuietly(in);
         }
+    }
+
+    private static JacksonJsonProvider createProvider()
+    {
+        final JacksonJsonProviderFactory jacksonJsonProviderFactory = new JacksonJsonProviderFactory();
+        Class<? extends JacksonJsonProviderFactory> factoryClass = jacksonJsonProviderFactory.getClass();
+        JacksonJsonProvider provider = null;
+        try
+        {
+            try
+            {
+                provider = (JacksonJsonProvider) factoryClass.getMethod("create").invoke(jacksonJsonProviderFactory);
+            }
+            catch (NoSuchMethodException e)
+            {
+                try
+                {
+                    provider = (JacksonJsonProvider) factoryClass.getMethod("create", Iterable.class).invoke(jacksonJsonProviderFactory, emptyList());
+                }
+                catch (NoSuchMethodException ex)
+                {
+                    throw new IllegalStateException("Unable to find a way to create a jackson json provider");
+                }
+            }
+        }
+        catch (InvocationTargetException e)
+        {
+            throw new RuntimeException(e.getCause());
+        }
+        catch (IllegalAccessException e)
+        {
+            throw new RuntimeException(e);
+        }
+        return provider;
     }
 
     public static String write(Object object) throws IOException
@@ -60,7 +99,7 @@ public class JsonObjectMapper
 
     public static void write(Object object, OutputStream out) throws IOException
     {
-        new JacksonJsonProviderFactory().create().writeTo(object, object.getClass(), object.getClass(), null, MediaType.APPLICATION_JSON_TYPE, null, out);
+        createProvider().writeTo(object, object.getClass(), object.getClass(), null, MediaType.APPLICATION_JSON_TYPE, null, out);
     }
 
     public static void write(Object object, Writer writer) throws IOException
